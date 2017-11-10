a3038b4f5df4d35df45203bae746ec8295d62575
Pamela Frieze <pfrieze@gmail.com>
2017-11-10 19:33:50 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,9 +13,9 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brands)
-    @brands = brands
-    BRANDS << brands
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand
   end
 
 end
