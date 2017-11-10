ed9cb81f4a698b9d9dea5dce287fef3b8f5f1d90
Pamela Frieze <pfrieze@gmail.com>
2017-11-10 19:42:36 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand unless BRANDS.inlude? (brand)
+    BRANDS << brand unless BRANDS.include? (brand)
   end
 
   def cobble
